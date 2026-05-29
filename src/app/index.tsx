9537297ff910b64d783a22af4c7bd1091aeccd0a
Kingsley-codes <agbamkingsley@gmail.com>
2026-05-29 22:43:51 +0100

    fixed assets

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,9 @@
 import { useEffect } from "react";
-import { ActivityIndicator, View } from "react-native";
+import { ActivityIndicator, Image, View } from "react-native";
 import { useRouter } from "expo-router";
 import { useAuth } from "@/hooks/useAuth";
+
+const launchIcon = require("../../assets/favicon.png");
 
 export default function Index() {
   const router = useRouter();
@@ -15,6 +17,11 @@ export default function Index() {
 
   return (
     <View className="flex-1 items-center justify-center bg-vuior-dark-bg">
+      <Image
+        source={launchIcon}
+        style={{ width: 160, height: 116, marginBottom: 24 }}
+        resizeMode="contain"
+      />
       <ActivityIndicator color="#00b874" />
     </View>
   );
